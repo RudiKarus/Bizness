5a7dca3ab8ff2ce1cbbc5c8a32af79ae025ced5c
rudikarus <rudi@e-ld.de>
2024-04-27 06:21:56 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Bizness;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var wordlist = File.ReadAllLines("/usr/share/wordlists/rockyou.txt.gz");
+        foreach (var word in wordlist)
+        {
+            Console.WriteLine(word);
+        }
+        Console.WriteLine("Hello, World!");
+    }
+}
