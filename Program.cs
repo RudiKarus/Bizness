73625b7e9a21627238414c11893db91a898482d6
rudikarus <rudi@e-ld.de>
2024-04-28 00:15:42 +0100

    Compare salted wordlistentries with the looted hash;

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 ﻿namespace Bizness;
 using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var wordlist = File.ReadAllLines("/usr/share/wordlists/rockyou.txt.gz");
-        foreach (var word in wordlist)
-        {
-            Console.WriteLine(word);
-        }
-        Console.WriteLine("Hello, World!");
+        //string[] wordlist = File.ReadAllLines("/home/b0b24ce0/Downloads/rockyou.txt");
+        // foreach (string word in wordlist)
+        // {
+        //     var shaHash = SHA1.HashData(Encoding.UTF8.GetBytes(word.ToCharArray()));
+        //     var encodedHash = Encoding.UTF8.GetString(shaHash);
+        //     string result = Convert.ToHexString(shaHash).ToLower();
+        //     Console.WriteLine($"{result}");
+        // }
+        string hashToCompare = "uP0_QaVBpDWFeo8-dRzDqRwXQ2I";
+        var hashToCompareAsBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(hashToCompare));
+
+        string saltString = "d";
+        byte[] salt = Encoding.UTF8.GetBytes(saltString);
+        string passwordString = "monkeybizness";
+        byte[] password = Encoding.UTF8.GetBytes(passwordString);
+
+        var saltedPassword = passwordString.Concat(saltString).ToArray();
+        byte[] saltedPasswordHash = SHA1.HashData(Encoding.UTF8.GetBytes(saltedPassword));
+
+        string saltedPasswordHashToCompareAsBase64 = Convert.ToBase64String(saltedPasswordHash);
+        string result = String.Concat("$SHA", $"${saltString}", $"${saltedPasswordHashToCompareAsBase64}");
+
+        Console.WriteLine($"Result: {result}");
+
+        //     var encodedHash = Encoding.UTF8.GetString(shaHash);
+        //     string result = Convert.ToHexString(shaHash).ToLower();
+        //     Console.WriteLine($"{result}");
+
     }
 }
