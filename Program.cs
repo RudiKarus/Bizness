7a4c0f2c231fc556cae6259859088c12d95caf80
rudikarus <rudi@e-ld.de>
2024-04-30 19:08:41 +0100

    CleanUp

--- Program.cs
+++ Program.cs
@@ -2,39 +2,33 @@
 using System.IO;
 using System.Text;
 using System.Security.Cryptography;
-using System.Runtime.Intrinsics.Arm;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //string[] wordlist = File.ReadAllLines("/home/b0b24ce0/Downloads/rockyou.txt");
-        // foreach (string word in wordlist)
-        // {
-        //     var shaHash = SHA1.HashData(Encoding.UTF8.GetBytes(word.ToCharArray()));
-        //     var encodedHash = Encoding.UTF8.GetString(shaHash);
-        //     string result = Convert.ToHexString(shaHash).ToLower();
-        //     Console.WriteLine($"{result}");
-        // }
+        // Set properties, based on findings from the targetmachine:  
+        string salt = "d";
         string hashToCompare = "uP0_QaVBpDWFeo8-dRzDqRwXQ2I";
-        var hashToCompareAsBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(hashToCompare));
 
-        string saltString = "d";
-        byte[] salt = Encoding.UTF8.GetBytes(saltString);
-        string passwordString = "monkeybizness";
-        byte[] password = Encoding.UTF8.GetBytes(passwordString);
+        // Since SHA is based on a non-linear function, there is no decryption.
+        // For this reason we need to "HashAndCompare" the entries of our wordlist...
+        List<string> wordlist = new List<string>(File.ReadAllLines($"{System.Environment.CurrentDirectory}/rockyou.txt"));
+        foreach (string passwordToCompare in wordlist)
+        {
+            string saltAndPassword = string.Concat(salt, passwordToCompare);
+            byte[] hashFromSaltAndPassword = SHA1.HashData(Encoding.UTF8.GetBytes(saltAndPassword));
+            string hashAsBase64 = Convert.ToBase64String(hashFromSaltAndPassword);
 
-        var saltedPassword = passwordString.Concat(saltString).ToArray();
-        byte[] saltedPasswordHash = SHA1.HashData(Encoding.UTF8.GetBytes(saltedPassword));
+            // ... and bring it into the expected, comparable format :
+            string result = hashAsBase64.Replace("/", "_").Replace("+", "-").Replace("=", "");
 
-        string saltedPasswordHashToCompareAsBase64 = Convert.ToBase64String(saltedPasswordHash);
-        string result = String.Concat("$SHA", $"${saltString}", $"${saltedPasswordHashToCompareAsBase64}");
-
-        Console.WriteLine($"Result: {result}");
-
-        //     var encodedHash = Encoding.UTF8.GetString(shaHash);
-        //     string result = Convert.ToHexString(shaHash).ToLower();
-        //     Console.WriteLine($"{result}");
-
+            if (result == hashToCompare)
+            {
+                Console.WriteLine($"Password found: {passwordToCompare}");
+                break;
+            }
+        }
+        Console.WriteLine("Done.");
     }
 }
